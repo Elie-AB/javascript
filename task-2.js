f9989c74f4186ccc1261959ac56ae38919f3c1aa
elie.abdo.y7@gmail.com <elie.abdo.y7@gmail.com>
2023-07-25 16:52:22 +0300

    done

---
+++ task-2.js
@@ -0,0 +1,2 @@
+let name="elie";
+name="abdo"
---
+++ task-3.js
@@ -0,0 +1,15 @@
+const num1=12;
+var num2=5;
+var canpass="true";
+if(canpass==true){
+if(num1>=num2){
+    console.log("hello");
+}else{
+    console.log("bye"):
+}
+}
+else{
+    console.log("try again");
+}
+num2=10;
+console.log(num2);
