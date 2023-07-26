5f009ae14e47c4e5a69935cb678440bd4d3c4b85
elie.abdo.y7@gmail.com <elie.abdo.y7@gmail.com>
2023-07-26 10:38:04 +0300

    done

---
+++ td4/task-4.js
@@ -0,0 +1,8 @@
+var array1 =[8,11,15,16];
+var array2=[];
+var array3= new array();
+array2=["elie","abdo","riit","bekaa"];
+array3=["elie",8,"abdo",16];
+console.log("array1:"+array1);
+console.log("array2:"+array2);
+console.log("array2:"+array3);
