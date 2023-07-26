5b0563e3212f6cbe31a63d0b3687d0cc98040509
elie.abdo.y7@gmail.com <elie.abdo.y7@gmail.com>
2023-07-26 14:42:46 +0300

    done

--- td5/task-5.js
+++ td5/task-5.js
@@ -1,10 +1,11 @@
 let numbers =[5,8,11,4,7,2,0,20,43];
-for(let i=0,i<=numbers.length,i++){
+console.log(numbers);
+for(let i=0;i<=numbers.length;i++){
     let x=numbers[i];
     if(x%2==0){
         console.log(x + "is even");
     }
-    else if(!x%2==0){
+    else{
         console.log(x + "is odd");
     }
 }
